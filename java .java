4302fa3fe062857426f85746553e11f611aa292d
sudhir314 <Sahilsaini314on@gmail.com>
2025-11-12 16:26:45 +0530

    Initial project commit (clean)

---
+++ java .java
@@ -0,0 +1,35 @@
+
+import java.util.Set;
+import javax.sound.sampled.SourceDataLine;
+
+public class demo{
+ private int  age;
+ private  String name;
+
+ public int getage(){
+    return age ;
+ }
+public  void Setage( int a){
+    int age ;
+}
+public  String getname(){
+    return name ;
+}
+public void Setname(){
+String name;
+}
+
+
+public class demo2{
+public static void main(String[] args) {
+    
+
+    demo d = Optional.ofNullable(new demo();
+        d.Setage(a);age();
+        d.name="sahil";
+    
+        System.out.println(a.age)
+        System.out.println(a.name)
+}
+}
+}
